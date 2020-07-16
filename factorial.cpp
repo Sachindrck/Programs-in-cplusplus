b5677ca4faf615d251f2f8aeb5fec98b61d02f10
Sachin kumar <46827344+Sachindrck@users.noreply.github.com>
2020-07-16 09:07:09 +0530

    initial_commit

---
+++ factorial.cpp
@@ -0,0 +1,22 @@
+// Iterative code to find the factorial of a number.
+// number should be in between 1-25.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Welcome to factorial calculator!\n";
+    cout << "Enter the number: ";
+    long int n;
+    cin >> n;
+    
+    long long int fact = 1;
+    
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    cout << endl;
+    cout << n << "! = " << fact;
+    return 0;
+}
