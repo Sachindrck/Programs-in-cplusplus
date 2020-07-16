335bf6552e7d2e8b60443bc545eb57c99b3f3ac1
Sachin kumar <46827344+Sachindrck@users.noreply.github.com>
2020-07-16 09:17:57 +0530

    factors

---
+++ factors.cpp
@@ -0,0 +1,22 @@
+
+// Iterative code to find the factors of a number.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Welcome to program for finding factors!\n";
+    cout << "Enter the number: ";
+    long int n;
+    cin >> n;
+    
+    cout << "Factors of the number " << n << " are: ";
+    for (long int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
